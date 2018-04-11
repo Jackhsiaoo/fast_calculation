060435abdff93a02d26f826729ccadc8f571b275
Jackhsiaoo <36875958+Jackhsiaoo@users.noreply.github.com>
2018-04-11 11:44:27 +0800

    4/11_sort2_odd

--- compute_matrix.c
+++ compute_matrix.c
@@ -6,18 +6,19 @@ int main()
 {
 	clock_t t1, t2;				// variables for computing clocks 
 	double **A, *x, *b, T1;
-	int i, j, N=10000;
+	int i, j, N=10;
+	char *c;
+	int n;
+	n = 0x04010302;							// c[0]=(02) c[1]=(03) c[2]=(01) c[3]=(04)
+	c = &n;
 
 	//srand(time(NULL));    //produce randon different variables
 	srand(0);
-	printf("%d\n",rand());
-	printf("%d\n",rand());
-	printf("%d\n",rand());
+	
 	
 	printf("-----------------");
 
-	for(N=2000;N<=20000;N*=2)
-	{
+	
 		A = (double **) malloc( N * sizeof(double*) );
 		A[0] = (double *) malloc( N*N*sizeof(double));
 		for(i=1;i<N;++i) A[i] = A[i-1] + N;
@@ -44,11 +45,15 @@ int main()
 		t2 = clock();
 		T1 = (t2-t1)/(double) CLOCKS_PER_SEC;
 		printf("Matrix time vector :%f\n",T1);
-		free(b);
-		free(x);
-		free(A[0]);
-		free(A);	
-	} 
+		printf("each location of n: %d %d %d %d\n", c[0], c[1], c[2], c[3]); // c[0]: c這個記憶體位置的值
+		printf("*c %d\n", *c);	
+		float *f = &n;								// sizeof(float) = 4,
+	
+		// (0111 1111 1000 0000 0000 0000 ...0)
+		//     7 F    8    0 
+	
+		n = 0x7F800000;   // n ( 00 00 80 7F )
+		printf("n = %d, f = %e\n", n, *f);
 
 	return 0;
 } 
--- sort_test2.cpp
+++ sort_test2.cpp
@@ -3,10 +3,11 @@
 #include <omp.h>
 #include <math.h>
 #include <time.h>
-#define DEBUG  0
+#define DEBUG  1
 int quicksort1(int *x, int left, int right);
 int partition(int *x, int low, int high) ;
 void swap(int *x,int a, int b);
+int findK(int *x, int left, int right, int k, int prePart);
 int main(){
 	clock_t t1, t2;				// variables for computing clocks 
 	int *x, *y, s, p;
@@ -23,12 +24,11 @@ int main(){
 	{
 		printf("x[%d]=%d\n",i,x[i]);
 	}
+	int kv=N/2+1;
+	int medium = 0;
 	printf("******partition********\n");
-	printf("partiton= %d\n",partition(x, 0, N));
-	for(i=0;i<N;++i)
-	{
-		printf("x[%d]=%d\n",i,x[i]);
-	}
+	printf("meddle= %d\n", findK(x, 0, N,kv,-1) );
+
 	return 0;
 }
 int quicksort1(int *x, int left, int right)
@@ -88,31 +88,70 @@ int quicksort1(int *x, int left, int right)
 	}
 	
 }
-int partition(int *x, int low, int high)
+int partition(int *x, int left, int right)
  {  
 		
-        int tmp = x[low];  
-        int i = low, j = high;  
-        while (i < j) {  
-            while (i < j && x[j] >= tmp){  
-                j--;  
-            }  
-            while (i < j && x[i] <= tmp){  
-                i++;  
-            }  
-            swap(x, i, j);  
-        }  
-        x[low] = x[i];  
-        x[i] = tmp; 
+    int i, j, k;
+	int pivot, t;
+	
+	//  
+	if(left < right-1)
+	{
+		pivot = x[left];
+    	i = left+1;
+    	j = right-1;
+    	// 
+    	while(1)
+		{
+			while(i < right && pivot >= x[i]) i++; // 往右邊找到第一個  pivot <  x[i]  
+      		while(j >  left && pivot <  x[j]) j--; // 往左邊找到第一個  pivot >= x[j] 
+      		#if DEBUG
+			printf("%d %d %d\n", i,j,pivot);
+			#endif
+      		if(i>=j) break;
+      		swap(x,i,j);
+      		#if DEBUG2
+			for(k=left;k<right;++k)
+			{
+				printf("x[%d]=%d\n",k,x[k]);
+			}
+			system("pause");
+			#endif
+		} 
+        x[left] = x[j];
+        x[j] = pivot;
 		 
-        return i;  
-} 
-void swap(int *x, int i, int j) {  
-        if(i == j){  
+        return j;  
+	} 
+}
+int findK(int *x, int left, int right, int k, int prePart)
+{  
+        if(left >= right)
+		{  
+            return 0;  
+        }  
+        int pos = partition(x, left, right);  
+        int leftnumber = pos -left  + 1;//左???  
+        if(k > leftnumber)
+		{//中位?在分裂?右?，??分裂?作?下次迭代的prePart  
+            findK(x, pos + 1, right, k - leftnumber, pos);  
+        }  
+        else if(k < leftnumber)
+		{//中位?在分裂?左?，本次的prePart作?下次迭代的prePart  
+            findK(x, left, pos - 1, k, prePart);  
+        } 
+        else
+		{
+			return x[pos];
+		}
+}
+void swap(int *x, int i, int j) 
+{  
+        if(i == j){  	
             return;  
         }  
         int tmp = x[i];  
         x[i] = x[j];  
         x[j] = tmp;  
-    }
+}
 
